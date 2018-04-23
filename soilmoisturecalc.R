83c3b9350ea6795ad8da5c41b8a87033f02c7d24
Eric Ireland <eireland@student.unimelb.edu.au>
2018-04-23 16:24:39 +1000

    moved bawbawmoisturemeans calc to end.

--- soilmoisturecalc.R
+++ soilmoisturecalc.R
@@ -4,9 +4,9 @@ Baw_Baw_soil_moisture <- read_csv("2018-04-23_Baw_Baw_soil_moisture.csv")
 Baw_Baw_soil_moisture$soil_moist_g <- Baw_Baw_soil_moisture$trayandsoil_moist_g - Baw_Baw_soil_moisture$tray_weight_g
 Baw_Baw_soil_moisture2 <- na.omit(Baw_Baw_soil_moisture) 
 Baw_Baw_soil_moisture2$soil_dry_g <- Baw_Baw_soil_moisture2$trayandsoil_dry_g - Baw_Baw_soil_moisture2$tray_weight_g
-bawbawmoisturemeans <- aggregate(Baw_Baw_soil_moisture2[, 11:12], list(Baw_Baw_soil_moisture2$contents), mean)
 Baw_Baw_soil_moisture2$moisture_g <- Baw_Baw_soil_moisture2$soil_moist_g - Baw_Baw_soil_moisture2$soil_dry_g
 Baw_Baw_soil_moisture2$moisture_percent <- (Baw_Baw_soil_moisture2$moisture_g*100)/Baw_Baw_soil_moisture2$soil_dry_g
 Baw_Baw_soil_moisture2$wd <- (Baw_Baw_soil_moisture2$soil_moist_g - Baw_Baw_soil_moisture2$soil_dry_g) / Baw_Baw_soil_moisture2$soil_dry_g
 Baw_Baw_soil_moisture2$moist_3_g_OD_equivalent <- 3 * (1 + Baw_Baw_soil_moisture2$wd)
 Baw_Baw_soil_moisture2$moist_10_g_OD_equivalent <- 10 * (1 + Baw_Baw_soil_moisture2$wd)
+bawbawmoisturemeans <- aggregate(Baw_Baw_soil_moisture2[, 11:12], list(Baw_Baw_soil_moisture2$contents), mean)
