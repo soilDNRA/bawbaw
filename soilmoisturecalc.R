299d37a04b62c486f48ddb75938e252e8bc39e49
Eric Ireland <ericireland@Erics-MacBook-Pro.local>
2018-04-23 16:18:45 +1000

    calculate mean oven dried equivalents.

--- soilmoisturecalc.R
+++ soilmoisturecalc.R
@@ -4,7 +4,7 @@ Baw_Baw_soil_moisture <- read_csv("2018-04-23_Baw_Baw_soil_moisture.csv")
 Baw_Baw_soil_moisture$soil_moist_g <- Baw_Baw_soil_moisture$trayandsoil_moist_g - Baw_Baw_soil_moisture$tray_weight_g
 Baw_Baw_soil_moisture2 <- na.omit(Baw_Baw_soil_moisture) 
 Baw_Baw_soil_moisture2$soil_dry_g <- Baw_Baw_soil_moisture2$trayandsoil_dry_g - Baw_Baw_soil_moisture2$tray_weight_g
-bawbawmoisturemeans <- aggregate(Baw_Baw_soil_moisture2[, 6:7], list(Baw_Baw_soil_moisture2$contents), mean)
+bawbawmoisturemeans <- aggregate(Baw_Baw_soil_moisture2[, 11:12], list(Baw_Baw_soil_moisture2$contents), mean)
 Baw_Baw_soil_moisture2$moisture_g <- Baw_Baw_soil_moisture2$soil_moist_g - Baw_Baw_soil_moisture2$soil_dry_g
 Baw_Baw_soil_moisture2$moisture_percent <- (Baw_Baw_soil_moisture2$moisture_g*100)/Baw_Baw_soil_moisture2$soil_dry_g
 Baw_Baw_soil_moisture2$wd <- (Baw_Baw_soil_moisture2$soil_moist_g - Baw_Baw_soil_moisture2$soil_dry_g) / Baw_Baw_soil_moisture2$soil_dry_g
