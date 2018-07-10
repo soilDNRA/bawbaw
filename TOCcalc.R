4c1c30cd62ea2d6e5975fc7ff7793bd0d90194bf
Eric Ireland <eireland@student.unimelb.edu.au>
2018-07-10 14:33:31 +1000

    # remove "_HWC_diluted1to20" from sample names

--- TOCcalc.R
+++ TOCcalc.R
@@ -12,9 +12,7 @@ bawbaw_TOC_50to97 <- bind_rows(bawbaw_TOC_50to59, bawbaw_TOC_60to97)
 bawbaw_TOC <- bawbaw_TOC_50to97 %>% 
   select(X3,X4,X5) %>% 
   mutate(X5 = X5*20) # calculate original concentration
-#  rename(X3 = "sample_name", X4 = "sample_ID", X5="result_TOC_mgperlitre")
+
 rename(bawbaw_TOC, sample_name = X3, sample_ID = X4, TOC_mg_per_l = X5)
 
-
-# see https://stackoverflow.com/questions/40659005/using-readr-want-to-use-col-only-option
-# tt <- read_csv("outcome-of-care-measures.csv", col_types = cols_only(Hospital 30-Day Death (Mortality) Rates from Heart Attack=col_character()), n_max = 10)
+bawbaw_TOC$X3 <- gsub("_HWC_diluted1to20", "", bawbaw_TOC$X3) # remove "_HWC_diluted1to20" from sample names
