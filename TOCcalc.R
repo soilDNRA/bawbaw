5397b5984a97e1ef224e439320afd76edfe0f9e7
Eric Ireland <eireland@student.unimelb.edu.au>
2018-07-10 13:19:14 +1000

    select columns and send to new dataframe bawbaw_TOC

--- TOCcalc.R
+++ TOCcalc.R
@@ -2,6 +2,11 @@ library(readr)
 library(tidyverse)
 
 bawbaw_TOC_50to59 <- read_table2(col_names = FALSE, "TOC_results/2018-07-02_Baw_Baw_50to59_diluted_1to20.txt", skip = 13)
+bawbaw_TOC <- bawbaw_TOC_50to59 %>% 
+  select(X3,X4,X5)
+#  rename(X3 = "sample_name", X4 = "sample_ID", X5="result_TOC_mgperlitre")
+
+
 
 # see https://stackoverflow.com/questions/40659005/using-readr-want-to-use-col-only-option
 # tt <- read_csv("outcome-of-care-measures.csv", col_types = cols_only(Hospital 30-Day Death (Mortality) Rates from Heart Attack=col_character()), n_max = 10)
