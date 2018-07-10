0974a1163429842dcddcd5ea7b68905019b5d4a3
Eric Ireland <eireland@student.unimelb.edu.au>
2018-07-10 13:36:43 +1000

    combine both data files

--- TOCcalc.R
+++ TOCcalc.R
@@ -1,11 +1,14 @@
 library(readr)
 library(tidyverse)
 
+
 bawbaw_TOC_50to59 <- read_table2(col_names = FALSE, "TOC_results/2018-07-02_Baw_Baw_50to59_diluted_1to20.txt", skip = 13)
-bawbaw_TOC <- bawbaw_TOC_50to59 %>% 
+bawbaw_TOC_60to97 <- read_table2(col_names = FALSE, "TOC_results/2018-07-04_Baw_Baw_60to97_diluted_1to20.txt", skip = 13)
+bawbaw_TOC_50to97 <- bind_rows(bawbaw_TOC_50to59, bawbaw_TOC_60to97)
+bawbaw_TOC <- bawbaw_TOC_50to97 %>% 
   select(X3,X4,X5)
 #  rename(X3 = "sample_name", X4 = "sample_ID", X5="result_TOC_mgperlitre")
-
+rename(bawbaw_TOC, sample_name = X3, sample_ID = X4, TOC_mg_per_l = X5)
 
 
 # see https://stackoverflow.com/questions/40659005/using-readr-want-to-use-col-only-option
