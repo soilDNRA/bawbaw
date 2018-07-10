a1b74da155f2dc5dcaacd5edd9cedeabbdacfe0b
Eric Ireland <eireland@student.unimelb.edu.au>
2018-07-10 13:43:15 +1000

    calculate original concentration

--- TOCcalc.R
+++ TOCcalc.R
@@ -6,7 +6,8 @@ bawbaw_TOC_50to59 <- read_table2(col_names = FALSE, "TOC_results/2018-07-02_Baw_
 bawbaw_TOC_60to97 <- read_table2(col_names = FALSE, "TOC_results/2018-07-04_Baw_Baw_60to97_diluted_1to20.txt", skip = 13)
 bawbaw_TOC_50to97 <- bind_rows(bawbaw_TOC_50to59, bawbaw_TOC_60to97)
 bawbaw_TOC <- bawbaw_TOC_50to97 %>% 
-  select(X3,X4,X5)
+  select(X3,X4,X5) %>% 
+  mutate(X5 = X5*20) # calculate original concentration
 #  rename(X3 = "sample_name", X4 = "sample_ID", X5="result_TOC_mgperlitre")
 rename(bawbaw_TOC, sample_name = X3, sample_ID = X4, TOC_mg_per_l = X5)
 
